72d5d28140701008882af712ecc931605c4501c6
samir <sibrahim@administrators-MacBook-Pro-5.local>
2019-07-25 15:00:37 +0300

    add netfly url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  appUrl: 'http://localhost:8080/auth',
+  appUrl:
+    'https://5d3998ff6e2129bab818b7c0--boring-mclean-c6f9d3.netlify.com/auth',
   spotifyClientId: 'bcf9aaa1adc0444a8941fc9eae652fce'
 };
